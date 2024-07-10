186d35de52689bb7b7d5385cee429c5222b01b1d
Antoine Labalette <labalette.antoine@gmail.com>
2024-07-10 16:49:48 +0200

    logistic regression

---
+++ logreg_predict.py
@@ -0,0 +1,47 @@
+import sys
+import math
+import pandas as pd
+
+
+def hypothesis(beta0, beta1, x):
+    return 1 / (1 + math.exp(-(beta0 + beta1 * x)))
+
+
+def predict(beta0, beta1, x):
+    return hypothesis(beta0, beta1, x) >= 0.5
+
+
+def sort(line, betas):
+    if predict(betas[0], betas[1], line["Flying"]):
+        return "Gryffindor"
+    if predict(betas[2], betas[3], line["Divination"]):
+        return "Slytherin"
+    if predict(betas[4], betas[5], line["Charms"]):
+        return "Ravenclaw"
+    return "Hufflepuff"
+
+
+def main(filename, betas):
+    df = pd.read_csv(filename)
+    df_pred = pd.DataFrame()
+    df_pred["Index"] = df.index
+    df["Flying"] = (df["Flying"] - df["Flying"].mean()) / df["Flying"].std()
+    df["Divination"] = (df["Divination"] - df["Divination"].mean()) / df[
+        "Divination"
+    ].std()
+    df["Charms"] = (df["Charms"] - df["Charms"].mean()) / df["Charms"].std()
+    df_pred["Hogwarts House"] = df["Index"].map(lambda x: sort(df.loc[x], betas))
+
+    df_pred.to_csv("houses.csv", index=False)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        exit("Usage: python logreg_train.py dataset_test.csv weights.txt")
+
+    betas = []
+
+    with open("weights.txt") as f:
+        for _ in range(6):
+            betas.append(float(f.readline()))
+    main(sys.argv[1], betas)
---
+++ logreg_train.py
@@ -0,0 +1,70 @@
+import sys
+import pandas as pd
+import math
+
+
+class LogisticRegression:
+    def __init__(self, alpha=0.01, n_iter=1000):
+        self.alpha = alpha
+        self.n_iter = n_iter
+        self.beta0 = 0
+        self.beta1 = 0
+
+    def fit(self, x, y):
+        for _ in range(self.n_iter):
+            delta0 = 0
+            delta1 = 0
+            for i in range(len(x)):
+                delta0 += self.hypothesis(x[i]) - y[i]
+                delta1 += (self.hypothesis(x[i]) - y[i]) * x[i]
+            self.beta0 -= self.alpha * delta0 / len(x)
+            self.beta1 -= self.alpha * delta1 / len(x)
+
+    def hypothesis(self, x):
+        return 1 / (1 + math.exp(-(self.beta0 + self.beta1 * x)))
+
+    def predict(self, x):
+        return self.hypothesis(x).map(lambda x: 1 if x >= 0.5 else 0)
+
+    def get_params(self):
+        return [self.beta0, self.beta1]
+
+    def set_params(self, beta0, beta1):
+        self.beta0 = beta0
+        self.beta1 = beta1
+
+
+def format_data(df, col_name, house):
+    data = df.dropna(subset=[col_name])
+    x = data[col_name]
+    y = data["Hogwarts House"].map(
+        {"Ravenclaw": 0, "Slytherin": 0, "Gryffindor": 0, "Hufflepuff": 0, house: 1}
+    )
+    x = (x - x.mean()) / x.std()
+    return x.values.tolist(), y.values.tolist()
+
+
+def main(filename):
+    df = pd.read_csv(filename)
+    with open("weights.txt", "w") as f:
+        logreg = LogisticRegression()
+
+        x, y = format_data(df, "Flying", "Gryffindor")
+        logreg.fit(x, y)
+        f.write(f"{logreg.get_params()[0]}\n{logreg.get_params()[1]}\n")
+
+        logreg.set_params(0, 0)
+        x, y = format_data(df, "Divination", "Slytherin")
+        logreg.fit(x, y)
+        f.write(f"{logreg.get_params()[0]}\n{logreg.get_params()[1]}\n")
+
+        logreg.set_params(0, 0)
+        x, y = format_data(df, "Charms", "Ravenclaw")
+        logreg.fit(x, y)
+        f.write(f"{logreg.get_params()[0]}\n{logreg.get_params()[1]}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit("Usage: python logreg_train.py dataset_train.csv")
+    main(sys.argv[1])
