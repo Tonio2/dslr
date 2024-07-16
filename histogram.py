733705e4deaee97714337fc1445ae91ab446503f
Antoine Labalette <labalette.antoine@gmail.com>
2024-07-16 13:51:19 +0200

    histogram

--- histogram.py
+++ histogram.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-import pdb
+import numpy as np
 
 def get_unique_values(values):
     res = []
@@ -12,34 +12,36 @@ def get_unique_values(values):
     
     return res
 
+def parse_colum(df, col_name, house_names):
+    ranges = np.linspace(df[col_name].min(), df[col_name].max(), 10)
+    ret = pd.DataFrame(0, index=ranges, columns=house_names)
+    
+    for v in df.index:
+        for r in ranges:
+            if df[col_name][v] <= r:
+                ret.at[r, df['Hogwarts House'][v]] += 1
+                break
+    
+    return ret
+    
+
 def main(filename):
-    pdb.set_trace()
     df = pd.read_csv(filename)
     
     house_names = get_unique_values(df['Hogwarts House'])
     col_heads = [col_name for col_name in df.columns if df[col_name].dtypes == "float64"]
-    res = pd.DataFrame(0.0, index=house_names, columns=col_heads)
-    count = pd.DataFrame(0, index=house_names, columns=col_heads)
-    
-    for col_name in col_heads:
-        for i in range(len(df[col_name])):
-            if not pd.isnull(df[col_name][i]):
-                res.at[df['Hogwarts House'][i], col_name] += df[col_name][i]
-                count.at[df['Hogwarts House'][i],col_name] += 1
-        for house in house_names:
-            res.at[house, col_name] = res.at[house, col_name] / count.at[house, col_name]
     
     fig, axes = plt.subplots(nrows=5, ncols=3, figsize=(15, 25))
     axes = axes.flatten()
     
     colors = ['blue', 'green', 'red', 'yellow']
 
+    for i, column in enumerate(col_heads):
+        data = parse_colum(df, column, house_names)
+        ax=axes[i]
+        data.plot(kind='bar', ax=ax, title=column, color=colors, legend=False)
+        ax.set_xticklabels([int(label) for label in data.index], rotation=45, ha='right')
 
-    for i, column in enumerate(res.columns):
-        res[column].plot(kind='bar', ax=axes[i], title=column, color=colors)
-        axes[i].set_xlabel('')
-        axes[i].set_ylabel('Value')
-        axes[i].set_xticklabels([])  # Remove x-axis labels
 
         
     from matplotlib.patches import Patch
@@ -48,7 +50,8 @@ def main(filename):
     fig.legend(handles=legend_elements, loc='upper center', ncol=4, fontsize='large', title='Houses')
 
 
-    plt.tight_layout(rect=[15.0, 0.03, 1, 0.95])
+    plt.tight_layout(rect=[10.0, 0.03, 1, 0.95])
+    plt.subplots_adjust(hspace=0.7)
     plt.show()
 
 
